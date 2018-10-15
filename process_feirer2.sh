1050a1e7b616e8baaee899cd52f462f3b0b7f711
Leila Esmaeili <lesmaeili@dons.usfca.edu>
2018-10-15 02:47:12 +0000

    passed_travis check

--- process_feirer2.sh
+++ process_feirer2.sh
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-# !/bin/bash
+#!/bin/bash
 # Leila Esmaeili
 # HW6 _Fall_2018
 
@@ -8,7 +7,7 @@
 # exclude first line
 for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
 do
-   fastq-dump -v $SRA_number -O data/raw_data
+   fastq-dump -v "$SRA_number" -O data/raw_data
 done
 
 echo
@@ -32,8 +31,8 @@ sleep 2s
 echo " trim files"
 for f in data/raw_data/*; do
 	 B="$(cut -d'/' -f3 <<<"$f")"
-	echo $B
-	TrimmomaticSE -threads 2 -phred33 $f data/trimmed/$(basename -s .fastq $B).trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
+	echo "$B"
+	TrimmomaticSE -threads 2 -phred33 "$f" data/trimmed/"$(basename -s .fastq "$B")".trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
 done
 
 
@@ -50,8 +49,8 @@ echo " convert fastQ into  Fasta"
 for f in data/trimmed/*.fastq; do
 	B="$(cut -d'/' -f3 <<<"$f")"
 	C="$(cut -d'.' -f1 <<<"$B")"
-	bioawk -c fastx '{print ">"$name"\n"$seq}' $f > data/trimmed/$C.trim.fasta
-	echo $f 
+	bioawk -c fastx '{print ">"$name"\n"$seq}' "$f" > data/trimmed/"$C".trim.fasta
+	echo "$f" 
 done
 
 echo " finished converting"
@@ -64,14 +63,7 @@ echo "time for  Blast"
 for file in data/trimmed/*.fasta;
 do
  blastn -db /blast-db/nt -num_threads 2 -outfmt '10 sscinames std' -out output/Blast_results/"$(basename -s .fasta "$file").csv" -max_target_seqs 1 -negative_gilist /blast-db/2018-09-19_environmental_sequence.gi -query "$file"  
- echo "checking on one file"
+ echo "Checked one file, moving on to the next..."
 done 
 
-=======
-#!/bin/bash
-for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
-do
-    fastq-dump -v $SRA_number -O data/raw_data
-done
 
->>>>>>> c81d6d75bc36111e616bd8b3cec1b1c736134ba4
